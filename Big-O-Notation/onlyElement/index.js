edbb07f02c02780f976aac08789658e7e61e1693
pamykcharyyeva <pamyk.ch@gmail.com>
2020-10-12 15:51:42 -0500

    subtotal and onlyements functions

---
+++ Big-O-Notation/onlyElement/index.js
@@ -0,0 +1,12 @@
+function onlyElementsAtEvenIndex(array) {
+    var newArray = Array(Math.ceil(array.length / 2));
+    for (var i = 0; i < array.length; i++) {
+        if (i % 2 === 0) {
+            newArray[i / 2] = array[i];
+        }
+    }
+    return newArray;
+}
+
+
+// this is O(n) because its not nested loop even if there are so many operations 
